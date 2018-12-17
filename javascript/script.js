ca2e0a410a2651cc373c6028b7c63feb8808ef02
Addison Dalton <zarch14@gmail.com>
2018-12-17 00:14:41 -0500

    Added basic rotation to rotors.

--- javascript/script.js
+++ javascript/script.js
@@ -1,32 +1,42 @@
 /* 
 * id is simply the id of the rotor,
-* alphabet is the list of letters that will substitute a base letter. 
+* cipherLetters is the list of letters that will substitute a base letter. 
 * cipherArr is the 2D array of letter to cipherLetter relation
-* position is the current notch / postion the rotor is at
+* notchPosition is the current notch / postion the rotor is at starting at 0
+* machinePosition is the current position in the machine of the rotor. i.e. is it in position 1, 2, or 3.
 * turnOver is the notch / position at which this rotor causes the next rotor to rotate
 *
 */
 class Rotor {
-    constructor(id, alphabet, position, turnOver) {
+    constructor(id, cipherLetters, notchPosition, machinePosition, turnOver) {
         this.id = id;
-        this.alphabet = alphabet;
+        this.cipherLetters = cipherLetters;
+        this.defaultCipherLetters = cipherLetters;
         this.cipherArr = this.createSubstitutionCipher();
-        this.position = position;
+        this.notchPosition = notchPosition;
+        this.machinePosition = machinePosition;
         this.turnOver = turnOver;
     }
 
-    // takes an alphabet string for a rotor and creates a 2D array with a base (regular) alphabet with each letter's
-    // corresponding cipher letter, provided by the rotor's alphabet string.
+    // takes a cipherLetters string for a rotor and creates a 2D array with a base (regular) alphabet with each letter's
+    // corresponding cipher letter, provided by the rotor's cipherLetters string.
     createSubstitutionCipher(){
         var baseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         var cipherArr = [];
     
         for (i = 0; i < 26; i++) {
             var plainLetter = baseAlphabet.charCodeAt(i);
-            var cipherLetter = this.alphabet.charCodeAt(i);
+            var cipherLetter = this.cipherLetters.charCodeAt(i);
             cipherArr.push([plainLetter,cipherLetter]);
         }
         return cipherArr;
+    }
+
+    rotote() {
+        var movedLetter = this.cipherLetters[0];
+        this.cipherLetters = this.cipherLetters.slice(1,26) + movedLetter;
+        this.cipherArr = this.createSubstitutionCipher();
+        this.notchPosition = (this.notchPosition + 1) % 26;
     }
 }
 
@@ -89,6 +99,25 @@ function rotorProcess(charNum) {
     // var activeRotors = getActiveRotors(); returns array of active rotors
     rotors.forEach(function(rotor) {
         charNum = getCipherLetter(rotor.cipherArr, charNum);
+        switch (rotor.machinePosition) {
+            case 1:
+                rotor.rotote();
+                break;
+            case 2:
+                var rotorI = rotors[0];
+                if (rotorI.notchPosition == rotorI.turnOver) {
+                    rotor.rotote();
+                }
+                break;
+            case 3:
+                var rotorII = rotors[1];
+                if (rotorII.notchPosition == rotorII.turnOver) {
+                    rotor.rotote();
+                }
+                break;
+            default:
+                rotor.rotote();
+        }
     });
 
     return charNum;
@@ -96,15 +125,16 @@ function rotorProcess(charNum) {
 
 // creates the rotors.
 function getRotors() {
-    var rotorIAlphabet = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
-    var rotorIIAlphabet = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
-    var rotorIIIAlphabet = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
+    var rotorICipherLetters = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
+    var rotorIICipherLetters = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
+    var rotorIIICipherLetters = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
     
     // array of rotor objects
+    // rotor: (id, cipherLetters, notchPosition, machinePosition, turnOverCharNum)
     var rotors = [
-        rotorI = new Rotor(1, rotorIAlphabet, 1, 18),
-        rotorII = new Rotor(1, rotorIIAlphabet, 1, 6),
-        rotorIII = new Rotor(1, rotorIIIAlphabet, 1, 24)
+        rotorI = new Rotor(1, rotorICipherLetters, 0, 1, 17),
+        rotorII = new Rotor(1, rotorIICipherLetters, 0, 2, 5),
+        rotorIII = new Rotor(1, rotorIIICipherLetters, 0, 3, 23)
     ];
 
     return rotors;
